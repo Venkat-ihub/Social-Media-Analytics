ce156a44e568aedb2d282fc04792a47fad5373ed
Venkat-ihub <venkat.b.ihub@snsgroups.com>
2025-01-28 09:48:33 +0530

    Delete SOCHR directory

--- SOCHR/my_app/my_app/forms.py
+++
@@ -1,8 +0,0 @@
-# my_app/forms.py
-from django import forms
-from .models import JobPost
-
-class JobPostForm(forms.ModelForm):
-    class Meta:
-        model = JobPost
-        fields = ['title', 'description', 'media']
